ca6d6a1d501d3ee02e2b8223529a70cf55b5c3b6
AhmedSBelal <ahmadsd77777@gmail.com>
2025-02-21 22:32:48 +0200

    make some relations not all

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Supplier extends Model
 {
-    //
+
+    protected $fillable = [
+        'supplier_id',
+        'admin_id',
+        'phone_number',
+        'status',
+    ];
+
+    // relations
+    public function users(): BelongsTo {
+        return $this->belongsTo(User::class, 'admin_id', 'id');
+    }
+
+    public function admins(): BelongsTo {
+        return $this->belongsTo(Admin::class, 'admin_id', 'id');
+    }
+
 }
