e68117796b2d6d2130ae4be15890041b5ed8f0ad
Ahmed <Ahmed@gmail.com>
2025-03-05 00:07:30 +0200

    Implement sign-in , logout functionality with Sanctum and Remember Me feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable , HasApiTokens;
+    // use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -60,4 +62,21 @@ class User extends Authenticatable
         return $this->hasMany(Charity::class, 'charity_id', 'id');
     }
 
+    /**
+     * Auth With jWT
+    */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
